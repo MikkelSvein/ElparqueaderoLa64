45b973403bbfd3a9f63e58a3ae2662c3d9786195
SamuelRivas17 <samuel43t95@gmail.com>
2025-10-19 14:56:49 -0500

    Delete auth0.php

--- php/confi/auth0.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'domain' => 'TU_DOMINIO.auth0.com',
-    'client_id' => 'TU_CLIENT_ID',
-    'client_secret' => 'TU_CLIENT_SECRET',
-    'redirect_uri' => 'http://localhost:8000/callback.php',
-    'cookie_secret' => 'una_clave_larga_y_segura',
-];
